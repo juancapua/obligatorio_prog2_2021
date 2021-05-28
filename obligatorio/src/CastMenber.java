863230563ce6d6ff85f2ba44e19f40d524001fd6
juancapua <80767235+juancapua@users.noreply.github.com>
2021-05-28 10:17:49 -0300

    Add files via upload

---
+++ obligatorio/src/CastMenber.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class CastMenber {
+
+    private String imdbNameid;
+    private String name;
+    private String birthName;
+    private int height;
+    private String bio;
+    private Date birthDate;
+    private String birthState;
+    private String birthCountry;
+    private String birthCity;
+    private Date deathDate;
+    private String deathState;
+}
